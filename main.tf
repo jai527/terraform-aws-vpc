2250f7799fb0ef05c218e8485ec797938fbf1c29
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-10 09:24:21 +0530

    hi,

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_internet_gateway" "main" {
   tags = local.igw_final_tags
 }
 
-resource "aws_subnet" "public" {
+/* resource "aws_subnet" "public" {
     count    = length(var.public_cidr)
     vpc_id     = aws_vpc.main.id
     cidr_block = var.public_cidr[count.index]
@@ -26,4 +26,4 @@ tags = merge(
     }
   )
 
-}
+} */

d53e5bffe4748a782a4ea0041e3bb570a89fee74
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-12 22:58:48 +0530

    hi,

--- data.tf
+++ data.tf
@@ -11,3 +11,11 @@ data "aws_vpc" "default" {
 data "aws_route_tables" "default" {
   vpc_id = data.aws_vpc.default.id
 }
+
+data "aws_route_table" "default" {
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}

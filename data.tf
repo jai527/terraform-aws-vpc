1a511a067609ce0d53bb9c068f3129f2ce6aa3a5
Jayashankar Modili <jayashankar.modili@ibm.com>
2026-06-12 19:22:44 +0530

    hi,

--- data.tf
+++ data.tf
@@ -7,3 +7,16 @@ data "aws_vpc" "default" {
     default = true
   
 }
+
+data "aws_route_tables" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_route_table" "default" {
+    vpc_id = data.aws_vpc.default.id
+    filter {
+      name = "association.mian"
+      values = true
+    }
+  
+}
--- peering.tf
+++ peering.tf
@@ -9,3 +9,32 @@ count = var.is_peering_required ? 1 : 0
     Name = "${var.project}-${var.environment}-to-default"
   }
 }
+
+# iam peering with all but in real-time where we required there only we can apply vpc-peering
+
+resource "aws_route" "public_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+}
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+
+}
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+}
+
+resource "aws_route" "default_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.default.id
+  destination_cidr_block    = var.vpc_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+}
